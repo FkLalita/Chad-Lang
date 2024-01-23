3d7b44af70b0886e331ce1dcc8209faf3ce22150
FkLalita <faruq2142@gmail.com>
2024-01-23 04:53:06 +0100

    buiding parser

---
+++ parser/parser.go
@@ -0,0 +1,50 @@
+package parser
+
+import (
+	"github.com/FkLalita/Chad-Lang/ast"
+	"github.com/FkLalita/Chad-Lang/lexer"
+	"github.com/FkLalita/Chad-Lang/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.ParseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) ParseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
